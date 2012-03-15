5e466d5973e7e9882c22fdd7a2f0d7c900277d10
dtoad WAMP <dzetoad2@gmail.com>
2012-03-15 17:15:35 -0500

    text warnings on passwrd restrictions

--- createUser.php
+++ createUser.php
@@ -128,8 +128,9 @@ if(isset($statusLabel) && $statusLabel!=''){
 <div>
 <h4>Instructions:</h4>
 <p>
-Passwords must be 8 characters long and include at least one letter and one number. 
-You must enter the characters displayed in the space below them then click on the "Register" button to register your username. 
+Passwords must be at least 8 characters long, include at least one letter and one number, and only be composed of alphanumerics and the following: !@#$%^\&amp;*   .
+ 
+You must enter the security code displayed in the space below them, then click on the "Register" button to register your username. 
 If you cannot read the characters, you may select a different image. After you have successfully created your Username and Password a confirming message 
 will appear on the screen. Save your access information for future access. To access the survey, click on Log in and enter your access information and click on Log In.
 </p>
